c0c6a99421e0e1e0865d633ef714ce6e547ea899
gablon29 <gabriellondero40@gmail.com>
2025-02-10 22:21:37 -0300

    mejorando la gestión de migraciones y optimizando la creación de usuarios con manejo de errores y validación

--- src/User/user.service.ts
+++ src/User/user.service.ts
@@ -1,4 +1,10 @@
-import { HttpException, Inject, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  HttpException,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, EntityManager, Repository } from 'typeorm';
 import { Users } from './user.entity';
@@ -9,46 +15,31 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(Users) private userRepository: Repository<Users>,
+    @InjectRepository(Users) private readonly userRepository: Repository<Users>,
     private dataSource: DataSource,
     @Inject(ConfigService) private configService: ConfigService,
   ) {}
 
   async createUser(user: Omit<UserDto, 'id'>): Promise<Users> {
-    const saltRound = this.configService.get<number>('PARSE_SALT_ROUND');
+    const saltRound = this.configService.get<number>('PARSE_SALT_ROUND', 10);
+    // verify if user exist
+    const userExist = await this.userRepository.findOne({
+      where: { username: user.username },
+    });
+    if (userExist) {
+      throw new ConflictException('User already exist');
+    }
+    // if not exist, create user.
+    const hashedPassword = await bcry.hash(user.password, saltRound);
+    const newUser = this.userRepository.create({
+      ...user,
+      password: hashedPassword,
+    });
     try {
-      return this.dataSource.transaction(async (manager: EntityManager) => {
-        const userExists = await this.userRepository.findOne({
-          where: { username: user.username },
-        });
-        if (userExists) {
-          throw new HttpException(
-            {
-              status: 400,
-              error: 'El usuario ya existe',
-            },
-            400,
-          );
-        } else {
-          const salt = await bcry.genSalt(saltRound);
-          const hashedPassword = await bcry.hash(user.password, salt);
-          const newUser = this.userRepository.create({
-            name: user.name,
-            username: user.username,
-            password: hashedPassword,
-          });
-          return manager.save(newUser);
-        }
-      });
+      return this.userRepository.save(newUser);
     } catch (error) {
       console.error(error);
-      throw new HttpException(
-        {
-          status: 400,
-          error: 'Error en la operacion',
-        },
-        400,
-      );
+      throw new InternalServerErrorException('Error creating user');
     }
   }
 
