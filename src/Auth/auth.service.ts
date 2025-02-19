d0d4569f47a2674da5a4fef56c718fd59b5d8fdc
gablon29 <gabriellondero40@gmail.com>
2025-02-19 19:34:20 -0300

    implementando la configuracion del token de acceso y actualizando los paquetes

--- src/Auth/auth.service.ts
+++ src/Auth/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService, JsonWebTokenError } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  login(user: any): string {
+  constructor(private readonly jwtService: JwtService) {}
+
+  public async validateUser(token: string): Promise<boolean> {
+    return false;
+  }
+
+  public login(user: any): string {
     return `Welcome ${user.name}!`;
   }
 }
